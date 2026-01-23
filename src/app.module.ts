0a99eeec9efc5696598c2e36a6562085ba72c2ce
LunalejaS <lunalejasandoval2007@gmail.com>
2026-01-22 20:25:58 -0500

    unificar estructura interna de los módulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { SubjectsService } from './modules/subjects/services/subjects.service';
-import { SubjectsController } from './modules/subjects/subjects.controller';
+import { SubjectsController } from './modules/subjects/controllers/subjects.controller';
 import { SubjectsModule } from './modules/subjects/subjects.module';
 import { NotificationsModule } from './modules/notifications/notifications.module';
 import { NotificationsController } from './modules/notifications/controllers/notifications.controller';
