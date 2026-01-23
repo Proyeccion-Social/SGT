6cc8d2b64a2040df8ce6166c1bf155768d807bd3
LunalejaS <lunalejasandoval2007@gmail.com>
2026-01-22 20:54:29 -0500

    cambio: separar dominios tutor y student desde users

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './controllers/users.controller';
-import { StudentsController } from './controllers/students.controller';
-import { TutorsController } from './controllers/tutors.controller';
-import { StudentsService } from './services/students.service';
-import { TutorsService } from './services/tutors.service';
+import { StudentsController } from '../student/controllers/student.controller';
+import { TutorsController } from '../tutor/controllers/tutor.controller';
+import { StudentsService } from '../student/services/student.service';
+import { TutorsService } from '../tutor/services/tutor.service';
 import { UsersService } from './services/users.service';
 
 
