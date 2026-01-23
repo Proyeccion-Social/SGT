b59321a591b13f55070091cc6169dbfc19e6534a
LunalejaS <lunalejasandoval2007@gmail.com>
2026-01-22 19:45:29 -0500

    cambio: creación de estructura base de módulos y carpetas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { SubjectsService } from './subjects/services/subjects.service';
-import { SubjectsController } from './subjects/subjects.controller';
-import { SubjectsModule } from './subjects/subjects.module';
-import { NotificationsModule } from './notifications/notifications.module';
-import { NotificationsController } from './notifications/controllers/notifications.controller';
-import { SchedulingModule } from './scheduling/scheduling.module';
-import { EvaluationModule } from './evaluation/evaluation.module';
-import { RatingQueryService } from './evaluation/services/rating-query.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { SubjectsService } from './modules/subjects/services/subjects.service';
+import { SubjectsController } from './modules/subjects/subjects.controller';
+import { SubjectsModule } from './modules/subjects/subjects.module';
+import { NotificationsModule } from './modules/notifications/notifications.module';
+import { NotificationsController } from './modules/notifications/controllers/notifications.controller';
+import { SchedulingModule } from './modules/scheduling/scheduling.module';
+import { EvaluationModule } from './modules/evaluation/evaluation.module';
+import { RatingQueryService } from './modules/evaluation/services/rating-query.service';
 
 
 @Module({
