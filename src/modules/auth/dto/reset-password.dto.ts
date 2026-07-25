276bd6fd141c7aa95fb6fa8f14a6be7d8244bd23
Carlos E <sangroniscarlosenrique1@gmail.com>
2026-07-25 11:23:36 -0500

    Mod: Synchronize: false en lugar de true

--- src/modules/auth/dto/reset-password.dto.ts
+++ src/modules/auth/dto/reset-password.dto.ts
@@ -4,10 +4,13 @@ import { IsString, MinLength, Matches } from 'class-validator';
 export class ResetPasswordDto {
   @IsString()
   @MinLength(8)
-  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]/, {
-    message:
-      'Contraseña debe incluir mayúsculas, minúsculas, números y caracteres especiales',
-  })
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]/,
+    {
+      message:
+        'Contraseña debe incluir mayúsculas, minúsculas, números y caracteres especiales',
+    },
+  )
   password: string;
 
   @IsString()
